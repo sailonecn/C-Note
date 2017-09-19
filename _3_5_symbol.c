02afdf4816894c02517221870f7b30e463ff976d
Rudy <netlink@outlook.com>
2017-09-19 08:07:44 +0800

    Create _3_5_symbol.c
    摘自《程序员的自我修养》3.5

---
+++ _3_5_symbol.c
@@ -0,0 +1,22 @@
+/*
+  弱引用判断程序链接到的是单线程库还是多线程库
+*/
+
+#include <stdio.h>
+#include <pthread.h>
+
+int pthread_create( pthread_t *, const pthread_attr_t *, void* (*)(void*), void *) __attribute__ ((weak));
+
+int main()
+{
+  if (pthread_create)
+  {
+    //run the multi-thread version
+  }
+  else
+  {
+    //run the sigle-thread version
+  }
+  
+  return 0;
+}
